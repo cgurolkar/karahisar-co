19c8285b445c36d89122128816091899656a05b4
cgurolkar <cgurolkar@gmail.com>
2026-04-28 02:16:56 +0300

    sistem güncellemeleri

--- app/hakkimizda/page.tsx
+++ app/hakkimizda/page.tsx
@@ -112,7 +112,7 @@ export default function AboutPage() {
             <div className="bg-gray-50 rounded-xl p-8 border border-gray-100 flex flex-col items-center text-center">
               <div className="mb-4 rounded-lg bg-black p-4 shadow-sm w-full flex justify-center items-center min-h-[72px]">
                 <img
-                  src="/logos/igkfast-logo.svg"
+                  src="/logos/igkfast-logo-591.png"
                   alt="IGKfast"
                   className="h-10 w-auto object-contain"
                 />
